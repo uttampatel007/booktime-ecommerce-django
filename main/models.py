ef4076cf1e464540f51d16b57cea27666b4c72e9
Uttam Velani <uttam11.velani11@gmail.com>
2021-03-05 11:52:02 +0530

    development: commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+	name = models.CharField(max_length=40)
+	description = models.TextField(blank=True)
+	price = models.DecimalField(max_digits=6, decimal_places=2)
+	slug = models.SlugField(max_length=50)
+	active = models.BooleanField(default=True)
+	in_stock = models.BooleanField(default=True)
+	date_updated = models.DateTimeField(auto_now=True)
+
+
+class ProductImage(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	image = models.ImageField(upload_to="product-images")
